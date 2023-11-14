55a328efdd23cda4fdae77919c530c6b84388611
Anthony Lara <Capone0824@gmail.com>
2023-11-13 23:44:29 -0500

    added functions to save tags and new notes

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,14 +1,20 @@
-import { NoteData } from "./App";
+import { NoteData, Tag } from "./App";
 import NoteForm from "./NoteForm";
 
-const NewNote = () => {
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[]
+};
+
+const NewNote = ({ onSubmit, onAddTag, availableTags }: NewNoteProps) => {
   return (
     <>
       <h1 className="mb-4">New Note</h1>
       <NoteForm
-        onSubmit={function (data: NoteData): void {
-          throw new Error("Function not implemented.");
-        }}
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
       />
     </>
   );
