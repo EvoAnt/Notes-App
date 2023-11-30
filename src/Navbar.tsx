55f16a10a6d4b4cdeee373e75124ecb15ac4d3b3
Anthony Lara <Capone0824@gmail.com>
2023-11-29 23:28:00 -0500

    more navabr updates

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,11 +1,12 @@
-import { Container, Nav, NavbarBrand, NavbarToggle } from "react-bootstrap";
+import { Container, Nav, NavLink, NavbarBrand, NavbarToggle } from "react-bootstrap";
 
 const Navbar = () => {
   return (
-    <Nav className="bg-primary">
+    <Nav className="bg-secondary mb-4 p-4">
       <Container>
         <NavbarBrand href='#home'>Notes App</NavbarBrand>
         <NavbarToggle aria-controls="basic-navbar-nav" />
+        
       </Container>
     </Nav>
   );
